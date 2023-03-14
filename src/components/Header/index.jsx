b4365ee157f231073653f0890c811751d8cb4637
Vinicius <viniciusmartinsdacunha1@gmail.com>
2023-03-14 01:07:12 -0300

    header mobile

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,11 +1,23 @@
-import { Container } from "./styles";
-import { Button } from "../Button";
+import { Container, Logo, Cart } from "./styles";
 import menuBurguer from "../../assets/icons/menu.svg";
+import logo from "../../assets/svgs/explorer.svg";
+import cart from "../../assets/icons/receipt.svg";
 
 export function Header() {
   return (
     <Container>
-      <Button primary>{menuBurguer}</Button>
+      <button>
+        <img src={menuBurguer} />
+      </button>
+
+      <Logo>
+        <img src={logo} />
+        <h1>food explorer</h1>
+      </Logo>
+
+      <Cart>
+        <img src={cart} />
+      </Cart>
     </Container>
   );
 }
--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -2,7 +2,32 @@ import styled from "styled-components";
 
 export const Container = styled.header`
   width: 100vw;
-  height: 114px;
+  max-height: 114px;
+
+  padding: 56px 28px 24px 28px;
 
   background-color: ${({ theme }) => theme.COLORS.DARK_700};
+
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  > button {
+    background-color: transparent;
+    border: none;
+    cursor: pointer;
+  }
 `;
+
+export const Logo = styled.div`
+  display: flex;
+  gap: 10px;
+
+  > h1 {
+    color: ${({ theme }) => theme.COLORS.LIGHT_100};
+    font-family: ${({ theme }) => theme.FONTS.POPPINS};
+    font-size: 24px;
+  }
+`;
+
+export const Cart = styled.div``;
