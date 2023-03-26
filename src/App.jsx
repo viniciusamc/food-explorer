f286dc9b06da6f58dbd14cd3ac5771f48cd2ae0d
Vinicius <viniciusmartinsdacunha1@gmail.com>
2023-03-26 18:48:27 -0300

    slider fail

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,56 @@ import { Card } from "./components/Card/input";
 import { Header } from "./components/Header";
 import salada from "./assets/meals/salada.svg";
 import { Carousel } from "./components/Carousel";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
 
 function App() {
   return (
+    <>
+      <Swiper
+        spaceBetween={50}
+        slidesPerView={3}
+        onSlideChange={() => console.log("slide change")}
+        onSwiper={(swiper) => console.log(swiper)}
+      >
+        <SwiperSlide>
+          {" "}
+          <Card
+            image={salada}
+            altImage="salada"
+            title="Salada"
+            value="15,00"
+          />{" "}
+        </SwiperSlide>
+        <SwiperSlide>
+          {" "}
+          <Card
+            image={salada}
+            altImage="salada"
+            title="Salada"
+            value="15,00"
+          />{" "}
+        </SwiperSlide>
+        <SwiperSlide>
+          {" "}
+          <Card
+            image={salada}
+            altImage="salada"
+            title="Salada"
+            value="15,00"
+          />{" "}
+        </SwiperSlide>
+        <SwiperSlide>
+          {" "}
+          <Card
+            image={salada}
+            altImage="salada"
+            title="Salada"
+            value="15,00"
+          />{" "}
+        </SwiperSlide>
+      </Swiper>
+    </>
   );
 }
 
