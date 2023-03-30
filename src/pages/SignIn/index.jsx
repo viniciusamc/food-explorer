22f6d663c912f567513b32af8322128a0c659a42
Vinicius <viniciusmartinsdacunha1@gmail.com>
2023-03-30 15:42:13 -0300

    feat: signup mobile

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -1,21 +1,33 @@
-import { Container, Logo, Form } from "./styles";
+import { Container, Logo, Form, Content, Link } from "./styles";
 import { Input } from "../../components/Input";
 import { Button } from "../../components/Button";
-// import { Link } from "react-router-dom";
+
+import logo from "../../assets/svgs/explorer.svg";
 
 export function SignIn() {
   return (
     <Container>
-      <Logo></Logo>
+      <Logo>
+        <img src={logo} />
+        <h1>food explorer</h1>
+      </Logo>
       <Form>
-        <label>Email</label>
-        <Input placeholder="Exemplo: exemplo@exemplo.com.br"></Input>
-        <label>Senha</label>
-        <Input placeholder="No mínimo 6 caracteres" />
+        <Content>
+          <label>Seu nome</label>
+          <Input placeholder="Exemplo: Maria da Silva"></Input>
+        </Content>
+        <Content>
+          <label>Email</label>
+          <Input placeholder="Exemplo: exemplo@exemplo.com.br"></Input>
+        </Content>
+        <Content>
+          <label>Senha</label>
+          <Input placeholder="No mínimo 6 caracteres" />
+        </Content>
 
         <Button text={"Entrar"} />
 
-        {/* <Link to="#">Não tem uma conta? Cadastre-se</Link> */}
+        <Link>Já tenho uma conta</Link>
       </Form>
     </Container>
   );
--- src/pages/SignIn/styles.js
+++ src/pages/SignIn/styles.js
@@ -1,15 +1,57 @@
 import styled from "styled-components";
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  min-height: 100vh;
 
-export const Logo = styled.div``;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Logo = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
+
+  color: ${({ theme }) => theme.COLORS.LIGHT_100};
+  font-family: ${({ theme }) => theme.FONTS.POPPINS};
+
+  margin-bottom: 73px;
+`;
 
 export const Form = styled.form`
   display: flex;
   flex-direction: column;
+  justify-content: center;
+  margin: 0 auto;
+  gap: 32px;
 
-  width: 90vw;
+  width: 80vw;
   max-width: 500px;
 
+  @media (min-width: 768px) {
+    background-color: ${({ theme }) => theme.COLORS.DARK_700};
+    border-radius: 16px;
+    padding: 64px;
+  }
+
   color: ${({ theme }) => theme.COLORS.LIGHT_100};
+`;
+
+export const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+
+  font-size: 16px;
+`;
+
+export const Link = styled.a`
+  color: ${({ theme }) => theme.COLORS.LIGHT_100};
+  font-family: ${({ theme }) => theme.FONTS.POPPINS};
+  font-weight: 500;
+
+  text-align: center;
 `;
